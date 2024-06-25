2cd74d6c7ac781dae57009fc980bf246b96ff5fd
prashant1177 <prashantjpatil49@gmail.com>
2024-06-25 18:00:13 +0530

    added edit and update feature for the listing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/travel";
 main().then(()=>{
@@ -19,7 +20,7 @@ async function main(){
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
-
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res)=>{
     res.send("hello");
@@ -41,6 +42,20 @@ app.get("/listings/:id", async (req, res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/show.ejs", {listing});
+});
+
+// Edit route
+app.get("/listings/:id/edit", async (req, res)=>{
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("./listings/edit.ejs", {listing});
+});
+
+// Update route
+app.put("/listings/:id", async (req, res)=>{
+    let {id} = req.params;
+    await Listing.findByIdAndUpdate(id, {...req.body.listing});
+    res.redirect(`/listings/${id}`);
 });
 
 // Save Route
