5ebd47143a56abf290d42bc94a5040d73b0845ea
prashant1177 <prashantjpatil49@gmail.com>
2025-02-09 16:17:04 +0530

    Added some features and fixed some issues, Such as redirecting to booking details after booking, added hotel conntact details, minor design improvement

--- controllers/booking.js
+++ controllers/booking.js
@@ -1,5 +1,6 @@
 const Booking = require("../models/booking");
 const Listing = require("../models/listing");
+const User = require("../models/user");
 // Create a new booking
 exports.createBooking = async (req, res) => {
   try {
@@ -11,7 +12,10 @@ exports.createBooking = async (req, res) => {
     const from = new Date(fromDate);
     const to = new Date(toDate);
     const totalDays = (to - from) / (1000 * 3600 * 24);
-    const totalPrice = Number(hotel.price * totalDays);
+    let totalPrice = hotel.price;
+    if (totalDays > 0) {
+      totalPrice = Number(totalPrice * totalDays);
+    }
     const newBooking = new Booking({
       hotel: hotel._id,
       user,
@@ -22,9 +26,8 @@ exports.createBooking = async (req, res) => {
       totalPrice,
     });
     await newBooking.save();
-
     req.flash("success", "Booking created successfully!");
-    res.redirect(`/listings/${hotel._id}`);
+    res.redirect(`/bookings/details/${newBooking._id}`);
   } catch (err) {
     req.flash("error", err.message);
     res.redirect("/");
@@ -43,20 +46,18 @@ exports.getBookings = async (req, res) => {
 
 exports.getBooked = async (req, res) => {
   const { id } = req.params;
-  try { const hotels = await Listing.find({ owner: id }).select('_id'); // Get only the hotel IDs
+  try {
+    const hotels = await Listing.find({ owner: id }).select("_id"); // Get only the hotel IDs
 
+    // Step 2: Find bookings for these hotels by searching for the 'hotel' field in the booking model
+    const bookings = await Booking.find({
+      hotel: { $in: hotels.map((hotel) => hotel._id) }, // Find bookings for the hotels owned by this owner
+    })
+      .populate("hotel") // Populate hotel details in the booking
+      .populate("user"); // Populate user details in the booking
 
-  // Step 2: Find bookings for these hotels by searching for the 'hotel' field in the booking model
-  const bookings = await Booking.find({
-    hotel: { $in: hotels.map(hotel => hotel._id) } // Find bookings for the hotels owned by this owner
-  })
-    .populate("hotel") // Populate hotel details in the booking
-    .populate("user"); // Populate user details in the booking
-
- 
-
-  // Step 3: Render the bookings page with the fetched bookings
-  res.render("./booking/booked.ejs", { bookings });
+    // Step 3: Render the bookings page with the fetched bookings
+    res.render("./booking/booked.ejs", { bookings });
   } catch (err) {
     res.status(500).send("Server Error");
   }
@@ -67,7 +68,8 @@ exports.getBookingDetails = async (req, res) => {
     const bookings = await Booking.findById(req.params.id)
       .populate("hotel")
       .populate("user");
-    res.render("./booking/bookingdetails.ejs", { bookings });
+      const owner = await User.findById(bookings.hotel.owner);
+    res.render("./booking/bookingdetails.ejs", { bookings,owner });
   } catch (err) {
     res.status(500).send("Server Error");
   }
--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,38 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-      form.addEventListener('submit', event => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+          event.preventDefault();
+          event.stopPropagation();
         }
-  
-        form.classList.add('was-validated')
-      }, false)
-    })
-  })()
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+function calculateTotalPrice(price) {
+  const fromDate = document.getElementById("fromDate").value;
+  const toDate = document.getElementById("toDate").value;
+  const from = new Date(fromDate);
+  const to = new Date(toDate);
+  const totalDays = (to - from) / (1000 * 3600 * 24);
+  let totalPrice = price;
+  if (totalDays > 0) {
+    totalPrice = Number(totalPrice * totalDays);
+  }
+  document.getElementById("totalPrice").innerText = totalPrice;
+}
+
+// Dates 
